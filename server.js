906489af321a60b3826e33f3cc73f4ebc25ca3a1
Glitch (url-shortener-hm) <none>
2020-05-02 09:01:21 +0000

    🌼⛈ Checkpoint ./server.js:15367774/466

--- server.js
+++ server.js
@@ -33,12 +33,15 @@ app.get("/api/hello", function (req, res) {
   res.json({ greeting: "hello API" });
 });
 
-regEx = /(\b(https?|ftp|file)://)?[-A-Za-z0-9+&@#/%?=~_|!:,.;]+[-A-Za-z0-9+&@#/%=~_|]/
+const regEx = /((([A-Za-z]{3,9}:(?:\/\/)?)(?:[-;:&=\+\$,\w]+@)?[A-Za-z0-9.-]+(:[0-9]+)?|(?:www.|[-;:&=\+\$,\w]+@)[A-Za-z0-9.-]+)((?:\/[\+~%\/.\w-_]*)?\??(?:[-\+=&;%@.\w_]*)#?(?:[\w]*))?)/;
 
 app.post("/api/shorturl/new", (req, res, next) => {
   const original_url = req.body.url;
+  if(regEx.test(original_url) === false) {
+    next(error);
+  }
   const short_url = "";
-  res.json({original_url: original_url, short_url: short_url})
+  res.json({original_url: original_url, short_url: short_url, validurl: regEx.test(original_url)})
 });
 
 app.use((err, req, res, next) => {
